132aa633f62056760b5be395cf367076f0ae09a5
SHIGAYUYA <shiga-yuya-yr@ynu,jp>
2019-07-22 17:15:08 +0900

    heroku config

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'url2html/get'
   post 'url2html/get' => "url2html#post"
+  root 'url2html#get'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
